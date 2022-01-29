5628974ae17c9efa45bb91e26a1015f1d4737578
EhsanTgv <EhsanTgv@gmail.com>
2022-01-29 23:24:50 +0300

    Testing Android Components, @Before & @After

---
+++ app/src/androidTest/java/com/taghavi/unittestingpractice/ResourceComparerTest.kt
@@ -0,0 +1,37 @@
+package com.taghavi.unittestingpractice
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "UnitTestingPractice")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Different")
+        assertThat(result).isFalse()
+    }
+}
---
+++ app/src/main/java/com/taghavi/unittestingpractice/ResourceComparer.kt
@@ -0,0 +1,10 @@
+package com.taghavi.unittestingpractice
+
+import android.content.Context
+
+class ResourceComparer{
+
+    fun isEqual(context: Context,resId:Int,string: String):Boolean{
+        return context.getString(resId)==string
+    }
+}
